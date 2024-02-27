fe662b742722c05bdb20898645069f4a0b2aef39
Franz04Rony <ventocilla2p@hotmail.com>
2024-02-27 15:25:10 -0500

    fixing navBar styles

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { Github } from "../icons/icons.jsx"
 export const Navbar = () => {
   return (
     <nav className="flex justify-between items-center h-[100px]" >
-        <div></div>
+        <div className="w-[100px]"></div>
         <span className="pl-6 text-2xl"> Salary Predictor 💰</span>
         <a href="https://github.com/Franz04Rony/salary-prediction" target="_blank" className="text-[#E0DEF4] hover:text-[#C4A7E7]" aria-label="View source on Github">
             <Github width={100} height={100}/>
